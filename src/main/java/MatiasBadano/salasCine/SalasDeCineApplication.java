8a9e4f50aa677d18e715b7961f4001f0c8231c0e
Matias Badano <matiasss95@gmail.com>
2023-07-27 16:16:20 -0300

    Create

---
+++ src/main/java/MatiasBadano/salasCine/SalasDeCineApplication.java
@@ -0,0 +1,13 @@
+package MatiasBadano.salasCine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalasDeCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalasDeCineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MatiasBadano/salasCine/SalasDeCineApplicationTests.java
@@ -0,0 +1,13 @@
+package MatiasBadano.salasCine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasDeCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
